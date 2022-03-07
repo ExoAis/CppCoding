aaf38e0b6024af88b44fb2f10b7a89a58eeb2360
ExoAis <93680758+ExoAis@users.noreply.github.com>
2022-03-08 00:43:50 +0500

    Update main.cpp

--- Maps/main.cpp
+++ Maps/main.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 int main() {
 	map<string, string> contacts;
-	contacts["Darab"] = "0313-4374801";
-	contacts["Fariha"] = "0315-4913752";
-	contacts["Ahsan"] = "0300-4432704";
+	contacts["Tifa"] = "0048204839";
+	contacts["Aerith"] = "8593769951";
+	contacts["Cloud"] = "0493867376";
 	for (/*pair<string, string>*/auto element : contacts) {
 		cout << element.first << " = " << element.second << endl;
 	}
